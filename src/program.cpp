27072d105bfe5d190cda4a0186b05ed7b45578aa
folil2 <choyeon0980@gmail.com>
2021-04-12 18:53:17 +0900

    회전하는 여러 개의 큐브 그리기

--- src/program.cpp
+++ src/program.cpp
@@ -23,14 +23,24 @@ bool Program::Link(const std::vector<ShaderPtr>& shaders) {
     int success = 0;
     glGetProgramiv(m_program, GL_LINK_STATUS, &success);
    if (!success) {
-    char infoLog[1024];
-    glGetProgramInfoLog(m_program, 1024, nullptr, infoLog);
-    SPDLOG_ERROR("failed to link program: {}", infoLog);
-    return false;
+        char infoLog[1024];
+        glGetProgramInfoLog(m_program, 1024, nullptr, infoLog);
+        SPDLOG_ERROR("failed to link program: {}", infoLog);
+        return false;
   }
   return true;
 }
 
 void Program::Use() const{
     glUseProgram(m_program);
+}
+
+void Program::SetUniform(const std::string& name, int value) const {
+    auto loc = glGetUniformLocation(m_program, name.c_str());
+    glUniform1i(loc, value);
+}
+
+void Program::SetUniform(const std::string& name, const glm::mat4& value) const {
+    auto loc = glGetUniformLocation(m_program, name.c_str());
+    glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(value));
 }
